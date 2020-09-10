b46c36d1152e2ae7e1f4c4a963e9e328b8c3798e
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-10 10:58:49 -0500

    task#10: new view for user model

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 """ views init """
 from flask import Blueprint
+from models.user import User
 from models.city import City
 from models.state import State
 
@@ -8,6 +9,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 """Wildcard import views"""
 from api.v1.views.index import *
+from api.v1.views.users import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+# from api.v1.views.places import *
