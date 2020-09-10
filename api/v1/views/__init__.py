4da9f87489b60a6468bc888b6e540f9c5062447a
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-10 12:39:15 -0500

    task#12: init file updated with reviews dependencies

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.user import User
 from models.city import City
 from models.state import State
+from models.review import Review
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -12,5 +13,6 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-from api.v1.views.amenities import *
 from api.v1.views.places import *
+from api.v1.views.reviews import *
+from api.v1.views.amenities import *
