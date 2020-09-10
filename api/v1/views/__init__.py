dd322b65daa2c3ce57f20d36f251fc5a24460604
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-09 21:29:24 -0500

    task#8: init file updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 """ views init """
 from flask import Blueprint
+from models.city import City
+from models.state import State
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -7,3 +9,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 """Wildcard import views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
